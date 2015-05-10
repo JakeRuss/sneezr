e9435ffbc77c021f88236cb842938c6c179b0de3
Jake Russ <jake.w.russ@gmail.com>
2015-05-10 12:30:30 -0400

    Update function to move dependencies into description.

--- R/pollencast.R
+++ R/pollencast.R
@@ -1,14 +1,15 @@
-library("zipcode")
-library("httr")
-library("jsonlite")
-library("stringr")
-library("dplyr")
-library("lubridate")
+#'Retrieve pollen count forecast
+#'
+#'@param zip five digit zip code
+#'@examples
+#'df  <- pollencast(zip = 20238)
+#'df2 <- pollencast(zip = "20238")
+#'@export
 
 pollencast <- function(zip = NULL) {
 
   # Format zip code
-  cleaned_zip <- clean.zipcodes(zips = zip)
+  cleaned_zip <- zipcode::clean.zipcodes(zips = zip)
 
   # Throw an error for zip codes returned as NA or not 5 digits
   if (nchar(cleaned_zip != 5) && is.na(cleaned_zip)) {
@@ -20,7 +21,7 @@ pollencast <- function(zip = NULL) {
   app_url  <- "weatherpollenservice/weatherpollenservice.svc/getforecast/"
   api_url  <- paste0(base_url, app_url, zip)
 
-  res <- GET(url = api_url)
+  res <- httr::GET(url = api_url)
 
   # Throw an error if the API returns non 200 status code
   stop_for_status(res)
@@ -28,33 +29,33 @@ pollencast <- function(zip = NULL) {
   # Create a list of data from returned json
   tmp_json <- res %>%
     # Extract the data from response object
-    content() %>%
+    httr::content() %>%
     # Parse the json into nested list
-    fromJSON() %>%
+    jsonlite::fromJSON() %>%
     # Un-nest and create data frames
     lapply(., data.frame, stringsAsFactors = FALSE)
 
   # Tidy the pollen count data
   pollen <- tmp_json$pollenForecast %>%
-    mutate(city          = tolower(city),
-           state         = tolower(state),
-           pp            = str_trim(pp),
-           timestamp     = parse_date_time(x      = timestamp,
-                                           orders = "%m%d,%Y%I%M!%S%p"),
-           forecast.date = seq.Date(from = as.Date(timestamp[1]),
-                                    to   = as.Date(timestamp[1]) + 3,
-                                    by   = 1)) %>%
+    dplyr::mutate(city          = tolower(city),
+                  state         = tolower(state),
+                  pp            = stringr::str_trim(pp),
+                  timestamp     = lubridate::parse_date_time(x      = timestamp,
+                                                             orders = "%m%d,%Y%I%M!%S%p"),
+                  forecast.date = seq.Date(from = as.Date(timestamp[1]),
+                                           to   = as.Date(timestamp[1]) + 3,
+                                           by   = 1)) %>%
     dplyr::rename(predominant = pp, pollen.ts = timestamp, pollen.count = forecast)
 
   # Tidy weather forcast
   weather <- tmp_json$weatherForecast %>%
-    mutate(date          = parse_date_time(x      = date,
-                                           orders = "%m%d,%Y%I%M!%S%p"),
+    mutate(date          = lubridate::parse_date_time(x      = date,
+                                                      orders = "%m%d,%Y%I%M!%S%p"),
            city          = tolower(city),
            state         = tolower(state),
-           forecast.date = parse_date_time(x      = forecast.date,
-                                           orders = "%m%d,%Y%I%M!%S%p") %>% as.Date()) %>%
-    rename(weather.ts = date, temp.low = forecast.lowF, temp.high = forecast.highF,
+           forecast.date = lubridate::parse_date_time(x      = forecast.date,
+                                                      orders = "%m%d,%Y%I%M!%S%p") %>% as.Date()) %>%
+    dplyr::rename(weather.ts = date, temp.low = forecast.lowF, temp.high = forecast.highF,
            description.day = forecast.phraseDay, description.night = forecast.phraseNight)
 
   # Join pollen and weather data
