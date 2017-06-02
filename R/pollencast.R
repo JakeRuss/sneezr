ab2a2a1887c108db35ee8708bbab43f49a36749b
Jake Russ <jake.w.russ@gmail.com>
2017-06-02 11:43:16 -0400

    Refactor pollencast to use Pollen.com's API directly

--- R/pollencast.R
+++ R/pollencast.R
@@ -1,12 +1,13 @@
 #'Retrieve pollen count forecast
 #'
 #'@param zip five digit zip code
+#'@param user.agent user agent string (optional); otherwise uses httr's default user agent
 #'@examples
 #'df  <- pollencast(zip = 20238)
 #'df2 <- pollencast(zip = "20238")
 #'@export
 
-pollencast <- function(zip = NULL) {
+pollencast <- function(zip = NULL, user.agent = NULL) {
 
   # Format zip code
   cleaned_zip <- zipcode::clean.zipcodes(zips = zip)
@@ -16,61 +17,57 @@ pollencast <- function(zip = NULL) {
     stop("Please supply a valid 5-digit zip code.")
   }
 
-  # Build the url for Claritin's API
-  base_url <- "http://www.claritin.com/"
-  api_url  <- paste0(base_url, "webservice/allergyforecast.php")
+  # Build the API url for Pollen.com
+  api_url <- paste0("https://www.pollen.com/api/forecast/current/pollen/", cleaned_zip)
 
-  res <- httr::POST(url = api_url, query = list(zip = cleaned_zip))
+  # Pollen.com validates against the referer header
+  pollen_headers <- c(
+               `Content-Type`    = "application/json; charset=utf-8",
+               Referer           = paste0("https://www.pollen.com/forecast/current/pollen/", cleaned_zip),
+               Host              = "www.pollen.com",
+               Connection        = "keep-alive",
+               `Accept-Language` = "en-US,en;q=0.8",
+               `Accept-Encoding` = "gzip, deflate, sdch, br",
+               Accept            = "application/json, text/plain, */*")
+
+  # Set the user_agent flexibly
+  if (is.null(user.agent)) {
+    ua <- httr::user_agent(httr:::default_ua())
+  } else {
+    ua <- httr::user_agent(user.agent)
+  }
+
+  # Make the GET request using supplied headers and api_url
+  res <- httr::GET(url = api_url, httr::add_headers(.headers = pollen_headers), ua)
 
   # Throw an error if the API returns non 200 status code
   httr::stop_for_status(res)
 
-  # Create a list of data from returned json
-  tmp_json <- res %>%
+  # Create a list of data from returned json via jsonlite
+  json_content <- res %>%
     # Extract the data from response object
     httr::content(as = "text") %>%
-    # Claritin's json source is double encoded (serialized?) which means
-    # we need to parse the json text twice.
-    jsonlite::fromJSON() %>%
-    jsonlite::fromJSON() %>%
-    # Un-nest and create data frames
-    lapply(., data.frame, stringsAsFactors = FALSE)
+    jsonlite::fromJSON()
 
-  # Tidy the pollen count data
-  pollen <- tmp_json$pollenForecast %>%
-    tidyr::unnest(pollen.count = forecast) %>%
-    dplyr::rename(predominant  = pp,
-                  pollen.ts    = timestamp) %>%
-    dplyr::mutate(city          = tolower(city)  %>% stringr::str_trim(),
-                  state         = tolower(state) %>% stringr::str_trim(),
-                  predominant   = stringr::str_trim(predominant),
-                  pollen.ts     = lubridate::parse_date_time(
-                                   x      = pollen.ts,
-                                   orders = "%m%d,%Y%I%M!%S%p"),
-                  forecast.date = seq.Date(from = as.Date(pollen.ts[1]),
-                                           to   = as.Date(pollen.ts[1]) + 3,
-                                           by   = 1))
+  # Tidy data from list to a data frame
+  tidy_pollen_data <- dplyr::data_frame(
+    forecast.date       = json_content$ForecastDate %>% as.Date(),
+    zip.code            = json_content$Location$ZIP,
+    city                = json_content$Location$City,
+    state               = json_content$Location$State,
+    pollen.date         = json_content$Location$periods$Type,
+    pollen.count        = json_content$Location$periods$Index,
+    pollen.trigger      = json_content$Location$periods$Triggers[[1]]$Name      %>% paste(collapse = ", "),
+    pollen.trigger.type = json_content$Location$periods$Triggers[[1]]$PlantType %>% paste(collapse = ", ")) %>%
 
-  # Tidy weather forcast
-  weather <- tmp_json$weatherForecast$forecast[[1]] %>%
-    dplyr::mutate(city          = tolower(tmp_json$weatherForecast$city) %>%
-                                    stringr::str_trim(),
-                  state         = tolower(tmp_json$weatherForecast$state) %>%
-                                    stringr::str_trim(),
-                  date          = substring(date, 1, 10) %>% as.Date(),
-                  weather.ts    = date[1] %>% as.Date()) %>%
-    dplyr::rename(forecast.date = date, temp.low = lowF, temp.high = highF,
-                  description.day = phraseDay, description.night = phraseNight)
+    # Manage the dates; note the case_when syntax, works for dplyr 0.5, will change in dplyr 0.6
+    dplyr::mutate(pollen.date = dplyr::case_when(.$pollen.date %in% "Today"     ~ as.character(.$forecast.date),
+                                                 .$pollen.date %in% "Yesterday" ~ as.character(.$forecast.date - 1),
+                                                 .$pollen.date %in% "Tomorrow"  ~ as.character(.$forecast.date + 1)),
+                  pollen.date = as.Date(pollen.date))
 
-  # Join pollen and weather data
-  combined <- pollen %>%
-    dplyr::inner_join(x = ., y = weather, by = c("city", "state", "forecast.date")) %>%
-  # Extract and re-order columns
-  dplyr::select(forecast.date, zip, city, state, pollen.count, predominant, pollen.ts,
-                temp.high, temp.low, description.day, description.night, weather.ts)
-
-  # Return the tidy data
-  return(combined)
+  # Return the tidied data
+  tidy_pollen_data
 }
 
 
