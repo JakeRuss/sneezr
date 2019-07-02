1936bb0b0a916f6d14e978bd3fa349afd99f8c44
Jake Russ <jake.w.russ@gmail.com>
2019-07-02 14:13:21 -0400

    Refactor pollencast to use Accuweather API

--- R/pollencast.R
+++ R/pollencast.R
@@ -1,13 +1,22 @@
 #'Retrieve pollen count forecast
 #'
 #'@param zip five digit zip code
+#'@param api_key API key string from Accuweather
+#'@param forecast_days desired number of forecast days; 1 by default, accepts 1, 5, 10, and 15
+#'@param metric Use metric system? defaults to "false"
+#'@param language defaults to US English, see Accuweather API docs for supported languages
 #'@param user_agent user agent string (optional); otherwise uses httr's default user agent
 #'@examples
 #'df  <- pollencast(zip = 20238)
 #'df2 <- pollencast(zip = "20238")
 #'@export
 
-pollencast <- function(zip = NULL, user_agent = NULL) {
+pollencast <- function(zip           = NULL,
+                       api_key       = NULL,
+                       forecast_days = 1,
+                       metric        = "false",
+                       language_code = "en-us",
+                       user_agent    = NULL) {
 
   # Format zip code
   cleaned_zip <- zipcode::clean.zipcodes(zips = zip)
@@ -17,28 +26,43 @@ pollencast <- function(zip = NULL, user_agent = NULL) {
     stop("Please supply a valid 5-digit zip code.")
   }
 
-  # Build the API url for Pollen.com
-  api_url <- paste0("https://www.pollen.com/api/forecast/current/pollen/", cleaned_zip)
+  # Throw an error if API key not supplied
+  if (is.null(api_key)) {
+    stop("Please supply an Accuweather API key")
+  }
 
-  # Pollen.com validates against the referer header
-  pollen_headers <- c(
-               `Content-Type`    = "application/json; charset=utf-8",
-               Referer           = paste0("https://www.pollen.com/forecast/current/pollen/", cleaned_zip),
-               Host              = "www.pollen.com",
-               Connection        = "keep-alive",
-               `Accept-Language` = "en-US,en;q=0.8",
-               `Accept-Encoding` = "gzip, deflate, sdch, br",
-               Accept            = "application/json, text/plain, */*")
+  # Throw an error is forecast_days is not acceptable number
+  if (!(forecast_days %in% c(1, 5, 10, 15))) {
+    stop("Forecast days must be one of 1, 5, 10, or 15")
+  }
+
+  # Throw an error if metric is true or false
+  if (!(tolower(metric) %in% c("true", "false"))) {
+    stop("Metric must be either 'true' or 'false'")
+  }
+
+  # Build the API url for Accuweather.com
+  api_url <- paste0(
+    "http://dataservice.accuweather.com/forecasts/v1/daily/",
+    forecast_days,
+    "day/",
+    cleaned_zip,
+    "?apikey=",
+    api_key,
+    "&language=",
+    language_code,
+    "&details=true&metric=",
+    metric)
 
   # Set the user_agent flexibly
-  if (is.null(user.agent)) {
+  if (is.null(user_agent)) {
     ua <- httr::user_agent(httr:::default_ua())
   } else {
-    ua <- httr::user_agent(user.agent)
+    ua <- httr::user_agent(user_agent)
   }
 
   # Make the GET request using supplied headers and api_url
-  res <- httr::GET(url = api_url, httr::add_headers(.headers = pollen_headers), ua)
+  res <- httr::GET(url = api_url, ua)
 
   # Throw an error if the API returns non 200 status code
   httr::stop_for_status(res)
@@ -49,22 +73,19 @@ pollencast <- function(zip = NULL, user_agent = NULL) {
     httr::content(as = "text") %>%
     jsonlite::fromJSON()
 
-  # Tidy data from list to a data frame
-  tidy_pollen_data <- dplyr::data_frame(
-    forecast.date       = json_content$ForecastDate %>% as.Date(),
-    zip.code            = json_content$Location$ZIP,
-    city                = json_content$Location$City,
-    state               = json_content$Location$State,
-    pollen.date         = json_content$Location$periods$Type,
-    pollen.count        = json_content$Location$periods$Index,
-    pollen.trigger      = json_content$Location$periods$Triggers[[1]]$Name      %>% paste(collapse = ", "),
-    pollen.trigger.type = json_content$Location$periods$Triggers[[1]]$PlantType %>% paste(collapse = ", ")) %>%
+  forecast_dates <- json_content$DailyForecasts$Date
 
-    # Manage the dates; note the case_when syntax, works for dplyr 0.5, will change in dplyr 0.6
-    dplyr::mutate(pollen.date = dplyr::case_when(.$pollen.date %in% "Today"     ~ as.character(.$forecast.date),
-                                                 .$pollen.date %in% "Yesterday" ~ as.character(.$forecast.date - 1),
-                                                 .$pollen.date %in% "Tomorrow"  ~ as.character(.$forecast.date + 1)),
-                  pollen.date = as.Date(pollen.date))
+  tidy_pollen_data <- json_content$DailyForecasts$AirAndPollen %>%
+    rlang:::set_names(forecast_dates) %>%
+    dplyr::bind_rows(.id = "forecast_date") %>%
+    dplyr::mutate(forecast_date    = readr::parse_datetime(
+                    x      = forecast_date,
+                    format = "%Y-%m-%dT%H:%M:%S%z"
+                  ),
+                  zip_code         = cleaned_zip,
+                  forecast_created = as.POSIXlt(Sys.time(), tz = "UTC") %>% as.POSIXct()) %>%
+    janitor::clean_names() %>%
+    dplyr::select(zip_code, dplyr::everything())
 
   # Return the tidied data
   tidy_pollen_data
